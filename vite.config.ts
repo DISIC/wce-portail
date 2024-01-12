ddadb2df949a6c0b9ebdb49b13f70b1c937402a4
youssef.el-mkhantar <youssef.el-mkhantar@i-carre.net>
2024-01-12 14:54:47 +0100

    fix pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        icons: [
-          {
-            src: '/dsfr/favicon/favicon.ico',
-            sizes: '512*512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
+    react()
   ],
   assetsInclude: ['**/*.md'],
   server: {
