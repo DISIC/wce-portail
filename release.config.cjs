95af0b00c8fba1d141dd2b18286850210286de76
Hamza KHAIT <HamzaKhait@users.noreply.github.com>
2024-03-14 14:46:45 +0100

    feat: creating release tar.gz package

--- release.config.cjs
+++ release.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
-        "assets": ["dist/*.js", "dist/*.js.map"],
+        "assets": ["wce-portail.tar.gz"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
