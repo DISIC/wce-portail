274ea2448436070ffc60ee67b5e004d3e5dbc537
mahfoudh <mahfoud.hafdi@apitech.fr>
2025-06-12 17:10:23 +0200

    add startVideo & checkVideo & handleRecordingStatus functions

--- src/pages/Jitsi_meet/Jitsi_meet.tsx
+++ src/pages/Jitsi_meet/Jitsi_meet.tsx
@@ -6,6 +6,8 @@ import styles from './jitsi_meet.module.css';
 import jwt_decode from 'jwt-decode';
 import { ReactNode } from 'react';
 import api from '../../axios/axios';
+import { useRef } from 'react';
+import Swal from 'sweetalert2';
 
 type errorObj = {
   message: string;
@@ -46,6 +48,143 @@ const Jitsi_meet = ({
   const handleReadyToClose = () => {
     navigate('/feedback');
   };
+
+  //visioreplay------------------------------------------------
+  const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+  const checkVideoInterval = useRef<NodeJS.Timeout | null>(null);
+  const checkTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const showLoadingToast = (message: string) => {
+    Swal.fire({
+      title: message,
+      showCloseButton: true,
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      didOpen: () => {
+        Swal.showLoading();
+      },
+    });
+  };
+
+  const startVideo = async () => {
+    const conference_name = roomName;
+
+    const status = "started";
+    const message = "Utilisateur commence l'enregistrement";
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/visioreplay/start_recording`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ status, message, conference_name }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("Replay créé avec status:", result);
+      } else {
+        const errorData = await response.json();
+        console.error("Erreur :", errorData);
+      }
+    } catch (error) {
+      console.error("Erreur :", error);
+    }
+  };
+
+  const checkVideo = async () => {
+    const conference_name = roomName!;
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/api/visioreplay/findReplay/${encodeURIComponent(conference_name)}`);
+      const data = await response.json();
+
+      if (data === "terminated") {
+        Swal.fire({
+          title: 'Succès !',
+          text: `La vidéo pour "${conference_name}" a été enregistrée avec succès.`,
+          icon: 'success',
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          showCloseButton: true
+        });
+
+        if (checkVideoInterval.current) clearInterval(checkVideoInterval.current);
+        if (checkTimeout.current) clearTimeout(checkTimeout.current);
+        checkVideoInterval.current = null;
+        checkTimeout.current = null;
+
+      } else if (data === "error-uploading-rsync") {
+        Swal.fire({
+          title: 'Erreur !',
+          text: `Une erreur est survenue lors de l'enregistrement de la vidéo pour "${conference_name}". Veuillez contacter le support.`,
+          icon: 'error',
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          showCloseButton: true
+        });
+
+        if (checkVideoInterval.current) clearInterval(checkVideoInterval.current);
+        if (checkTimeout.current) clearTimeout(checkTimeout.current);
+        checkVideoInterval.current = null;
+        checkTimeout.current = null;
+      }
+
+    } catch (error) {
+      console.error('Erreur lors de la vérification du replay :', error);
+    }
+  };
+
+  const handleRecordingStatus = (api: any) => {
+    let isRecordingStarted = false;
+
+    api.addEventListener('recordingStatusChanged', (event: any) => {
+      const isRecordingOn = event.on;
+      const error = event.error;
+
+      console.info("Changement de statut d'enregistrement :", event);
+
+      if (isRecordingOn) {
+        console.info("Enregistrement démarré");
+        isRecordingStarted = true;
+        startVideo();
+
+      } else if (error) {
+        console.error(`Erreur d'enregistrement : ${error}`);
+
+      } else if (isRecordingStarted) {
+        console.info("Enregistrement arrêté");
+        showLoadingToast("Upload de l'enregistrement en cours ...");
+
+        if (!checkVideoInterval.current) {
+          checkVideoInterval.current = setInterval(() => {
+            console.log("Vérification en cours du statut du replay...");
+            checkVideo();
+          }, 1000);
+
+          checkTimeout.current = setTimeout(() => {
+            clearInterval(checkVideoInterval.current as NodeJS.Timeout);
+            checkVideoInterval.current = null;
+            checkTimeout.current = null;
+            Swal.fire({
+              title: 'Erreur !',
+              text: `Une erreur est survenue lors de l'enregistrement de la vidéo. Veuillez contacter le support.`,
+              icon: 'error',
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              showCloseButton: true
+            });
+          }, 600000); // 10 min
+        }
+      }
+    });
+  };
+  //----------------------------------------------------------
 
   function roomNameConstraintOk(roomName: string | undefined) {
     /**
@@ -126,25 +265,24 @@ const Jitsi_meet = ({
   }, [roomName]);
 
   return (
-    <>
-      <JitsiMeeting
-        domain={import.meta.env.VITE_JITSI_DOMAIN}
-        roomName={roomName as string}
-        jwt={jwt1 ? jwt1 : undefined}
-        spinner={renderSpinner}
-        // config={{
-        //   hideConferenceSubject: false,
-        // }}
-        onApiReady={externalApi => {
-          if (typeof (window as any).setupRenderer === 'function') {
-            (window as any).setupRenderer(externalApi, {});
-          }
-          //handleApiReady(externalApi);
-        }}
-        onReadyToClose={handleReadyToClose}
-        getIFrameRef={handleJitsiIFrameRef1}
-      />
-    </>
+    <JitsiMeeting
+      domain={import.meta.env.VITE_JITSI_DOMAIN}
+      roomName={roomName as string}
+      jwt={jwt1 ? jwt1 : undefined}
+      spinner={renderSpinner}
+      // config={{
+      //   hideConferenceSubject: false,
+      // }}
+      onApiReady={externalApi => {
+        if (typeof (window as any).setupRenderer === 'function') {
+          (window as any).setupRenderer(externalApi, {});
+        }
+        handleRecordingStatus(externalApi);
+        //handleApiReady(externalApi);
+      }}
+      onReadyToClose={handleReadyToClose}
+      getIFrameRef={handleJitsiIFrameRef1}
+    />
   );
 };
 
